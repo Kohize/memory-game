1f8160bb34ca0f3e16a3ebd06cf20769fa093b90
Kohize <kohi881@gmail.com>
2025-06-15 21:50:13 +0300

    start game button

--- src/features/StartingScreen/StartingScreen.jsx
+++ src/features/StartingScreen/StartingScreen.jsx
@@ -1,9 +1,10 @@
+import { Link } from 'react-router';
 import Difficulty from './ui/Difficulty';
 import Rules from './ui/Rules';
 
 export default function StartingScreen() {
   return (
-    <section className="flex flex-col gap-y-5">
+    <section className="flex flex-col gap-y-5 items-center">
       <div className="flex flex-col item-center justify-center">
         <span className="animate-bounce text-4xl text-center">🧠</span>
         <h1 className="text-5xl text-center">MemoryGame</h1>
@@ -11,6 +12,11 @@ export default function StartingScreen() {
       </div>
       <Rules />
       <Difficulty />
+      <Link
+        to={'game'}
+        className="p-3 font-semibold rounded-lg cursor-pointer hover:opacity-70 bg-indigo-700 text-white max-w-45 text-center">
+        Start the Game
+      </Link>
     </section>
   );
 }
