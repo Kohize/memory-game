8c0e3e973af52bcff95f284727d043d7d4e8fe4a
Kohize <kohi881@gmail.com>
2025-06-15 19:53:57 +0300

    components + routes + store

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { RouterProvider, createBrowserRouter } from 'react-router';
+
+let router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Menu />,
+  },
+  {
+    path: 'game',
+    element: <Game />,
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
