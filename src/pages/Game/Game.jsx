b834267fd2ae906e16793d5a9cc3ec0bbedf5a68
Kohize <kohi881@gmail.com>
2025-06-15 22:04:46 +0300

    fetch pokemon data

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function Game() {
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
+        const data = await response.json();
+        console.log(data);
+        return data;
+      };
+      fetchData();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }, []);
   return <div>Game</div>;
 }
