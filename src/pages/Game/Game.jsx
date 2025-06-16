cec20d716e4edbe31cda784f54fb547f1215e192
Kohize <kohi881@gmail.com>
2025-06-16 16:31:01 +0300

    getting data from api and render on game page

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,18 +1,55 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setGameArray } from '../../utils/gameSlice';
 
 export default function Game() {
+  const array = useSelector((state) => state.game.gameArray);
+  const dispatch = useDispatch();
+  const [pokemonDetail, setPokemonDetail] = useState([]);
+
   useEffect(() => {
     try {
       const fetchData = async () => {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
+        const response = await fetch(
+          'https://pokeapi.co/api/v2/pokemon?limit=40'
+        );
         const data = await response.json();
-        console.log(data);
-        return data;
+        dispatch(setGameArray(data.results));
       };
       fetchData();
     } catch (error) {
       throw new Error(error);
     }
-  }, []);
-  return <div>Game</div>;
+  }, [dispatch]);
+
+  useEffect(() => {
+    const getDetails = async () => {
+      const details = await Promise.all(
+        array.map(async (item) => {
+          const response = await fetch(item.url);
+          return await response.json();
+        })
+      );
+      setPokemonDetail(details);
+    };
+    getDetails();
+  }, [array]);
+  console.log(pokemonDetail);
+
+  return (
+    <div>
+      <h1>Memory Game</h1>
+      <ul>
+        {pokemonDetail.map((item) => (
+          <li>
+            <p>{item.name}</p>
+            <img
+              src={item.sprites.other['official-artwork'].front_default}
+              alt="pokemon image"
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
---
+++ src/utils/gameSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  difficulty: 'easy',
+  gameArray: [],
+  selectedId: [],
+};
+
+const gameSlice = createSlice({
+  name: 'game',
+  initialState,
+  reducers: {
+    setGameArray: (state, action) => {
+      state.gameArray = action.payload;
+    },
+    setSelectedId: (state, action) => {
+      state.selectedId.push(action.payload);
+    },
+  },
+});
+
+export const { setGameArray, setSelectedId } = gameSlice.actions;
+
+export default gameSlice.reducer;
