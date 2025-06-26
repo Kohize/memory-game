d8838e8598204a60c7236d1ef7ce6966a8673978
Kohize <kohi881@gmail.com>
2025-06-26 16:45:40 +0300

    loader class fix

--- src/features/MainGame/ui/Loader/Loader.jsx
+++ src/features/MainGame/ui/Loader/Loader.jsx
@@ -7,7 +7,7 @@ export default function Loader() {
       className="flex flex-col justify-center items-center py-20 gap-y-5">
       <svg
         aria-hidden="true"
-        class="w-12 h-12 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600 dark:fill-gray-200"
+        className="w-12 h-12 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600 dark:fill-gray-200"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg">
